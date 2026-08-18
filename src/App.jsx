ca6d2d9eedaa2cfb0d1966a205894f8c1b178e78
chiwo67 <chiworyan@gmail.com>
2026-08-18 10:27:59 +0300

    add Login route to ProductDetails component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import { CartProvider } from "./context/CartContext";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
-import ProductDetails, { Overview, Reviews, Specs } from "./pages/ProductDetails";
+import ProductDetails, {
+  Overview,
+  Reviews,
+  Specs,
+} from "./pages/ProductDetails";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
@@ -16,11 +21,11 @@ export default function App() {
             <Route path="/" element={<Home />} />
             <Route path="/products" element={<Products />} />
             <Route path="/products/:id" element={<ProductDetails />}>
-              <Route path="overview" element={<Overview />} />
+            <Route path="overview" element={<Overview />} />
               <Route path="reviews" element={<Reviews />} />
               <Route path="specs" element={<Specs />} />
             </Route>
-           
+            <Route path="/login" element={<Login />} />
           </Routes>
         </BrowserRouter>
       </CartProvider>
